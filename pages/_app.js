8f4d2b1616d05b48dbf8b510b8ebf2924f75932b
AMIT KUMAR MAHATO <miamitkumar98@gmail.com>
2023-03-01 20:06:44 +0530

    Second Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+// import 'public/assets/js/main.js'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
