0cb92d7f04e9b0ded8621f989343e07d53bf42f3
shashankkrs <shashankrajput2812@gmail.com>
2023-04-01 22:35:06 +0530

    defectmemo pages

--- pages/admin/workshop/defectmemo.js
+++ pages/admin/workshop/defectmemo.js
@@ -10,6 +10,9 @@ import Router from "next/router";
 import DatalistInput from "react-datalist-input";
 import "react-datalist-input/dist/styles.css";
 import { Scrollbars } from 'react-custom-scrollbars';
+import { Button,Row } from 'react-bootstrap';
+import Link from "next/link";
+
 
 
 async function GetMemos() {
@@ -33,7 +36,6 @@ async function createDefectMemo(event) {
     condition_of_engine:event.target.condition_of_engine.value,
     signature:event.target.signature.value,
     designation:event.target.designation.value,
-    sl_no: event.target.sl_no.value,
     defect: event.target.defect.value,
     defect_reason: event.target.defect_reason.value,
     suggestion: event.target.suggestion.value,
@@ -43,7 +45,6 @@ async function createDefectMemo(event) {
     remarks:event.target.remarks.value
   };
 
-  //const formData = new FormData(event.target);
   console.log(data);
 
   const res = await axios({
@@ -57,12 +58,16 @@ async function createDefectMemo(event) {
 }
 
 export default function Home() {
-
-  function OpenLink(link) {
-    console.log(link);
-    Router.push("/admin/workshop/" + link);
-  }
-
+ const [memos, setMemos] = useState([]);
+    useEffect(() => {
+        GetMemos().then((data) => {
+            setMemos(data);
+        });
+    }, []);
+    function OpenLink(link) {
+        console.log(link);
+        Router.push('/admin/workshop/' + link);
+    }
   return (
     <>
       <Head>
@@ -73,13 +78,12 @@ export default function Home() {
       <main className={styles.main}>
         <Header />
         <SideBar />
-        <main id="main" className="col-lg-10 main">
-          
+        <main id="main" className="col-lg-10 main mt-0">
+          <Row>
           <div className="col-lg-7">
             <div className="card">
               <div className="card-body">
-              <Scrollbars style={{ width: 700, height: 600 }}>
-                <h1>Create Defect Memo</h1>
+              <h1>Create Defect Memo</h1>
                 <hr
                   style={{
                     color: "#000000",
@@ -88,23 +92,8 @@ export default function Home() {
                     padding: ".1rem",
                   }}
                 />
+              <Scrollbars style={{ width: 700, height: 600 }}>
                 <form onSubmit={createDefectMemo}>
-                
-                <div className="row mb-3">
-                    <label
-                      htmlFor="inputText"
-                      className="col-sm-5 col-form-label"
-                    >
-                    Sl No :
-                    </label>
-                    <div className="col-sm-5">
-                      <input
-                        type="number"
-                        name="sl_no"
-                        className="form-control"
-                      />
-                    </div>
-                  </div>
                     
                 <div className="row mb-3">
                     <label
@@ -373,12 +362,72 @@ export default function Home() {
                 </Scrollbars>
               </div>
             </div>
-
             <button 
-                  className="btn btn-primary"
-                  style={{ float: "left" }}                
-                  onClick={() => Router.back()}>Go Back</button>
+              className="btn btn-primary"
+              style={{ float: "left" }}                
+              onClick={() => Router.back()}>
+                Go Back
+                </button>
           </div>
+          <div className="col-lg-5">
+            <div className="card">
+              <div className="card-body">
+              <h1>Defect Memo List</h1>
+                <hr
+                  style={{
+                    color: "#000000",
+                    backgroundColor: "#000000",
+                    height: 1.5,
+                    padding: ".1rem",
+                  }}
+                />
+              <Scrollbars style={{ width: 490, height: 600 }}>
+              <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Sl No.</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Vehicle No</th>
+                                <th scope="col">KM Run</th>
+                                <th scope="col">Condition Of Engine</th>
+                            </tr>
+                        </thead>
+                        <tbody style={
+                            {cursor: "pointer"}
+                        }>
+                          {
+                            memos.map((memo, index) => {
+                              return (
+                                <tr key={
+                                      index + 1
+                                    }
+                                    onClick={
+                                        () => OpenLink(memo._id)
+                                    }>
+                                    <th scope="row">
+                                        {index+1}
+                                    </th>
+                                    <td>{
+                                        memo.date.substring(0,10)
+                                    }</td>
+                                    <td>{
+                                        memo.vehicle_no
+                                    }</td>
+                                    <td>{
+                                        memo.kilometers_run
+                                    }</td>
+                                    <td>{memo.condition_of_engine}</td>
+                                </tr>
+                              )
+                            })
+                          }  
+                        </tbody>
+                    </table>
+                  </Scrollbars>
+              </div>
+              </div>
+              </div>
+          </Row>          
         </main>
       </main>
       <Scripts />
